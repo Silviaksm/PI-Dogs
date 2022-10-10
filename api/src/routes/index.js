48d42eb53847c303a30cb5e801d4da28d72473de
silviaksm <sksalazarm@gmail.com>
2022-10-10 17:55:02 -0500

    Changes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogs = require('./dogs');
+// const temperament= require('./temperaments.js')
 
 const router = Router();
+
+router.use('./dogs', dogs);
+// router.use('./temperaments', temperament);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
