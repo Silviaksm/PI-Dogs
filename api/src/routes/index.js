05d595b1c7ec96813140d067ab3734d60eac7ed8
silviaksm <sksalazarm@gmail.com>
2022-10-26 17:52:20 -0500

    changes ok

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const axios = require('axios');
-const {Dog, Temperament}= require('../db');
-const {API_KEY}= process.env;
+const { Dog, Temperament } = require('../db');
+const { API_KEY } = process.env;
 
 
 const router = Router();
@@ -9,41 +9,48 @@ const router = Router();
 
 
 const getApiInfo = async () => {
-    const apiUrl = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
-    const apiInfo = await apiUrl.data.map(el => {
-        let temperamentArray = [];
-        if (el.temperament) {//pregunto que exista el temperamento y lo devuelvo en un arreglo
-            temperamentArray = el.temperament.split(", ");
-        }
+    try {
+        const apiUrl = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
+        const apiInfo = apiUrl.data.map(el => {
 
-        return {
-            id: el.id,
-            name: el.name,
-            height_min: el.height.metric.split(" - ")[0] && el.height.metric.split(" - ")[0],
-            height_max: el.height.metric.split(" - ")[1] && el.height.metric.split(" - ")[1],
-            weight_min: el.weight.metric.split(" - ")[0] && el.weight.metric.split(" - ")[0],
-            weight_max: el.weight.metric.split(" - ")[1] && el.weight.metric.split(" - ")[1],
-            temperaments: temperamentArray,
-            life_span_min: el.life_span.split(" - ")[0] && el.life_span.split(" - ")[0],
-            life_span_max: el.life_span.split(" - ")[1] && el.life_span.split(" - ")[1],
-            image: el.image.url,
-        }
-    })
-    
-    return apiInfo;
+            let temperamentArray = [];
+            if (el.temperament) {//pregunto que exista el temperamento y lo devuelvo en un arreglo
+                temperamentArray = el.temperament.split(", ");
+            }
+
+            return {
+                id: el.id,
+                name: el.name,
+                height_min: el.height.metric.split(" - ")[0] && el.height.metric.split(" - ")[0],
+                height_max: el.height.metric.split(" - ")[1] && el.height.metric.split(" - ")[1],
+                weight_min: el.weight.metric.split(" - ")[0] && el.weight.metric.split(" - ")[0],
+                weight_max: el.weight.metric.split(" - ")[1] && el.weight.metric.split(" - ")[1],
+                temperaments: temperamentArray,
+                life_span_min: el.life_span.split(" - ")[0] && el.life_span.split(" - ")[0],
+                life_span_max: el.life_span.split(" - ")[1] && el.life_span.split(" - ")[1],
+                image: el.image.url,
+
+            }
+        })
+
+        return apiInfo;
+    } catch (error) {
+        console.log(error)
+            ;
+    }
 }
 
-const getDbInfo = async ()=>{
+const getDbInfo = async () => {
     const dbDog = await Dog.findAll({
-        include:{
+        include: {
             model: Temperament,
             attributes: ['name'],
-            through:{
-                attributes:[],
+            through: {
+                attributes: [],
             },
         }
     })
-    const dogDb = await dbDog.map((e) => {
+    const dogDb = dbDog.map((e) => {
         return {
             id: e.id,
             name: e.name,
@@ -61,7 +68,7 @@ const getDbInfo = async ()=>{
     return dogDb
 };
 
-const getAllDogs = async ()=>{
+const getAllDogs = async () => {
     let apiInfo = await getApiInfo();
     let dbInfo = await getDbInfo();
     const infoTotal = apiInfo.concat(dbInfo);
@@ -69,75 +76,77 @@ const getAllDogs = async ()=>{
 }
 
 
-router.get("/dogs", async(req, res) => {
-   
+router.get("/dogs", async (req, res) => {
+
     const { name } = req.query;
     const allDogs = await getAllDogs();
     if (name) {
         const dog = allDogs.filter(d => d.name.toLowerCase().includes(name.toLowerCase()));
-        dog.length ? res.status(200).send(dog) : res.status(404).send("Dog not found"); 
+        dog.length ? res.status(200).send(dog) : res.status(404).send("Dog not found");
     } else {
         res.status(200).send(allDogs);
     }
 });
 
-router.get("/dogs/:id", async(req, res) => {
+router.get("/dogs/:id", async (req, res) => {
     const { id } = req.params;
     const allDogs = await getAllDogs();
     const dog = allDogs.filter(el => el.id == id);
     if (dog.length) {
         res.status(200).json(dog);
-    }else{
+    } else {
         res.status(404).send("Not found");
     }
 });
 
 router.get("/temperaments", async (req, res) => {
 
-const dbTemp = await Temperament.findAll();
-const dbUtilInfo = dbTemp.map((e) => e.name);
-try {
-  if (!dbTemp.length) {
-    let temp = [];
-    const apiData = await axios.get('https://api.thedogapi.com/v1/breeds');
-    const apiTemperaments = apiData.data.map(e => e.temperament).join(', ').split(', ')
-    apiTemperaments.map((e) => {
-        if (!temp.includes(e) && e !== "") {
-            temp.push(e)
+    const dbTemp = await Temperament.findAll();
+    const dbUtilInfo = dbTemp.map((e) => e.name);
+    try {
+        if (!dbTemp.length) {
+            let temp = [];
+            const apiData = await axios.get('https://api.thedogapi.com/v1/breeds');
+            const apiTemperaments = apiData.data.map(e => e.temperament).join(', ').split(', ')
+            apiTemperaments.map((e) => {
+                if (!temp.includes(e) && e !== "") {
+                    temp.push(e)
+                }
+
+            })
+            temp.forEach((e) => {
+                Temperament.findOrCreate({
+                    where: { name: e },
+                });
+            });
+            const dbTemps = await Temperament.findAll();
+            const utilInfo = dbTemps.map((e) => e.name);
+            res.status(200).send(utilInfo);
+        } else {
+            res.status(200).send(dbUtilInfo);
         }
-      
-    })
-    temp.forEach((e) => {
-      Temperament.findOrCreate({
-        where: { name: e },
-      });
-    });
-    const dbTemps = await Temperament.findAll();
-    const utilInfo = dbTemps.map((e) => e.name);
-    res.status(200).send(utilInfo);
-  } else {
-    res.status(200).send(dbUtilInfo);
-  }
-} catch (error) {
-  res.status(400).send("Temperament error");
-}
+    } catch (error) {
+        res.status(400).send("Temperament error");
+    }
 });
 
 
-router.post('/dogs', async (req,res)=>{
-    let{ name, height_min, height_max, weight_min, weight_max, life_span_min,life_span_max, image,createdInDb, temperament }= req.body
+router.post('/dogs', async (req, res) => {
+    let { name, height_min, height_max, weight_min, weight_max, life_span_min, life_span_max, image, createdInDb, temperament } = req.body
 
-    
 
-    let dogCreate= await Dog.create({
-        name, height_min, height_max, weight_min, weight_max, life_span_min,life_span_max, image ,createdInDb, temperament 
+
+    let dogCreate = await Dog.create({
+        name, height_min, height_max, weight_min, weight_max, life_span_min, life_span_max, image, createdInDb, temperament
     })
-    let temperamentDb= await Temperament.findAll({
-        where:{ name: temperament}
+    let temperamentDb = await Temperament.findAll({
+        where: { name: temperament }
     })
     dogCreate.addTemperaments(temperamentDb)
     res.send('Dog creado con exito')
 })
+
+
 
 
 module.exports = router;
